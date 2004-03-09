c24a286899f1770ef50afe2d923bfd3087704214
cignoni <paolo.cignoni@isti.cnr.it>
2004-03-09 14:17:58 +0000

    cr lf mismatch

--- vcg/simplex/vertex/with/n.h
+++ vcg/simplex/vertex/with/n.h
@@ -1,20 +1,21 @@
 #ifndef __VCGLIB_VERTEX__N__TYPE
 #define __VCGLIB_VERTEX__N__TYPE
 
+
 #define VERTEX_TYPE VertexN 
 
 #define __VCGLIB_VERTEX_N
 
 #include <vcg/simplex/vertex/base.h> 
 
-#undef VERTEX_TYPE 
 
+#undef VERTEX_TYPE 
 #undef __VCGLIB_VERTEX_N
 
-namespace vcg {
 
+namespace vcg {
 typedef VertexN<float>  VertexNf;
 typedef VertexN<double> VertexNd;
-
 }
+
 #endif
