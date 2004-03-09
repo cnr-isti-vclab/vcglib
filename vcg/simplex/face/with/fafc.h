417940a2d5d33404d48cf9a72a1a52f42adf47d6
cignoni <paolo.cignoni@isti.cnr.it>
2004-03-09 14:16:55 +0000

    Initial commit

---
+++ vcg/simplex/face/with/fafc.h
@@ -0,0 +1,16 @@
+#ifndef __VCGLIB_FACE_FAFC_TYPE
+#define __VCGLIB_FACE_FAFC_TYPE
+
+#define FACE_TYPE FaceFAFC 
+
+#define __VCGLIB_FACE_FA
+#define __VCGLIB_FACE_FC
+
+#include <vcg/simplex/face/base.h> 
+
+#undef FACE_TYPE 
+
+#undef __VCGLIB_FACE_FA
+#undef __VCGLIB_FACE_FC
+
+#endif
