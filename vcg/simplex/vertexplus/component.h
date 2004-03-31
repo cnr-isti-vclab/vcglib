2324d22a9589618e1f3d2e2a707f99e571b88bc4
ganovelli <fabio.ganovelli@isti.cnr.it>
2004-03-31 12:28:37 +0000

    *** empty log message ***

--- vcg/simplex/vertexplus/component.h
+++ vcg/simplex/vertexplus/component.h
@@ -24,6 +24,9 @@
   History
 
 $Log: not supported by cvs2svn $
+Revision 1.2  2004/03/29 14:26:38  cignoni
+Error in color
+
 Revision 1.1  2004/03/29 08:36:26  cignoni
 First working version!
 
@@ -31,6 +34,9 @@ First working version!
 ****************************************************************************/
 #ifndef __VCG_VERTEX_PLUS_COMPONENT
 #define __VCG_VERTEX_PLUS_COMPONENT
+
+#include <vcg/traced_vector.h>
+
 
 namespace vcg {
   namespace vert {
@@ -61,6 +67,17 @@ private:
 };
 template <class T> class Coord3f: public Coord<vcg::Point3f, T> {};
 template <class T> class Coord3d: public Coord<vcg::Point3d, T> {};
+
+template <class A, class T> class CoordOpt: public T {
+public:
+  typedef A CoordType;
+  typedef typename CoordType::ScalarType      ScalarType;
+	CoordType &P() { return CAT< TVector<VertType>,CoordType>::Get((VertType*)this); }
+  CoordType &UberP() { return CAT< TVector<VertType>,CoordType>::Get((VertType*)this); }
+};
+template <class T> class Coord3fOpt: public CoordOpt<vcg::Point3f, T> {};
+template <class T> class Coord3dOpt: public CoordOpt<vcg::Point3d, T> {};
+
 
 /*-------------------------- NORMAL ----------------------------------------*/ 
 
