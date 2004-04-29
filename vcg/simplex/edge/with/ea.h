f6c416728a4af13a3df4a7f2b17c77d693f2508a
ganovelli <fabio.ganovelli@isti.cnr.it>
2004-04-29 12:31:39 +0000

    created

---
+++ vcg/simplex/edge/with/ea.h
@@ -0,0 +1,14 @@
+#ifndef __VCGLIB_FACE_FA_TYPE
+#define __VCGLIB_FACE_FA_TYPE
+
+#define FACE_TYPE FaceFA 
+
+#define __VCGLIB_FACE_FA
+
+#include <vcg/simplex/face/base.h> 
+
+#undef FACE_TYPE 
+
+#undef __VCGLIB_FACE_FA
+
+#endif
