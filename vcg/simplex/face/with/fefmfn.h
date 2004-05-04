b032085a0eb449e22bd007aba68d2e3ab7012831
ganovelli <fabio.ganovelli@isti.cnr.it>
2004-05-04 02:40:47 +0000

    created

---
+++ vcg/simplex/face/with/fefmfn.h
@@ -0,0 +1,18 @@
+#ifndef __VCGLIB_FACE_EMN_TYPE
+#define __VCGLIB_FACE_EMN_TYPE
+
+#define FACE_TYPE FaceEMN
+
+#define __VCGLIB_FACE_E
+#define __VCGLIB_FACE_M
+#define __VCGLIB_FACE_N
+
+#include <vcg/simplex/face/base.h> 
+
+#undef FACE_TYPE 
+
+#undef __VCGLIB_FACE_E
+#undef __VCGLIB_FACE_M
+#undef __VCGLIB_FACE_N
+
+#endif
