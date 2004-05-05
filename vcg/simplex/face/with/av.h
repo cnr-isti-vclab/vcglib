99d05b351a7c76353dcbb1e8f2e687ed05750990
nicopietroni <nico.pietroni@isti.cnr.it>
2004-05-05 16:30:34 +0000

    with vertex-face topology

---
+++ vcg/simplex/face/with/av.h
@@ -0,0 +1,14 @@
+#ifndef __VCGLIB_FACE_AV_TYPE
+#define __VCGLIB_FACE_AV_TYPE
+
+#define FACE_TYPE FaceAV 
+
+#define __VCGLIB_FACE_AV
+
+#include <vcg/simplex/face/base.h> 
+
+#undef FACE_TYPE 
+
+#undef __VCGLIB_FACE_AV
+
+#endif
