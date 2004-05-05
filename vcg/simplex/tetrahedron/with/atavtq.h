14bc73eccbbdb44530931fc09a718856f25fbf81
nicopietroni <nico.pietroni@isti.cnr.it>
2004-05-05 12:15:16 +0000

    with tetrahedron - tetrahedron vertex - tetrahedron topology and quality

---
+++ vcg/simplex/tetrahedron/with/atavtq.h
@@ -0,0 +1,17 @@
+#ifndef __VCGLIB_TETRA_ATAVTQ_TYPE
+#define __VCGLIB_TETRA_ATAVTQ_TYPE
+
+#define TETRA_TYPE TetraATAVTQ 
+
+#define __VCGLIB_TETRA_AT
+#define __VCGLIB_TETRA_VT
+#define __VCGLIB_TETRA_TQ
+#include <vcg/simplex/tetrahedron/base.h> 
+
+#undef TETRA_TYPE 
+
+#undef __VCGLIB_TETRA_AT
+#undef __VCGLIB_TETRA_VT
+#undef __VCGLIB_TETRA_TQ
+
+#endif
