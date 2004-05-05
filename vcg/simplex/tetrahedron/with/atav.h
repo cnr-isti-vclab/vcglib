cf60d1aad9e90e6675dbe63e2f77fe0ad14a4683
nicopietroni <nico.pietroni@isti.cnr.it>
2004-05-05 12:05:35 +0000

    with tetrahedron - tetrahedron and vertex - tetrahedron topology

---
+++ vcg/simplex/tetrahedron/with/atav.h
@@ -0,0 +1,15 @@
+#ifndef __VCGLIB_TETRA_ATAV_TYPE
+#define __VCGLIB_TETRA_ATAV_TYPE
+
+#define TETRA_TYPE TetraATAV
+
+#define __VCGLIB_TETRA_AT
+#define __VCGLIB_TETRA_AV
+#include <vcg/simplex/tetrahedron/base.h> 
+
+#undef TETRA_TYPE 
+
+#undef __VCGLIB_TETRA_AT
+#undef __VCGLIB_TETRA_AV
+
+#endif
