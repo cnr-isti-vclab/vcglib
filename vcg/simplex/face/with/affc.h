dba60317fae40416e883017ce9691628e6a71d15
nicopietroni <nico.pietroni@isti.cnr.it>
2004-05-05 16:30:07 +0000

    with face topology and colour

---
+++ vcg/simplex/face/with/affc.h
@@ -0,0 +1,16 @@
+#ifndef __VCGLIB_FACE_AFFC_TYPE
+#define __VCGLIB_FACE_AFFC_TYPE
+
+#define FACE_TYPE FaceAFFC 
+
+#define __VCGLIB_FACE_AF
+#define __VCGLIB_FACE_FC
+
+#include <vcg/simplex/face/base.h> 
+
+#undef FACE_TYPE 
+
+#undef __VCGLIB_FACE_AF
+#undef __VCGLIB_FACE_FC
+
+#endif
