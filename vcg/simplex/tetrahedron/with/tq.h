3bd2e495b6bbad483eaf24488d3e15d270689cf3
nicopietroni <nico.pietroni@isti.cnr.it>
2004-05-05 13:09:37 +0000

    with quality

--- vcg/simplex/tetrahedron/with/tq.h
+++ vcg/simplex/tetrahedron/with/tq.h
@@ -1,14 +1,14 @@
-#ifndef __VCGLIB_TETRA_Q_TYPE
-#define __VCGLIB_TETRA_Q_TYPE
+#ifndef __VCGLIB_TETRA_TQ_TYPE
+#define __VCGLIB_TETRA_TQ_TYPE
 
-#define TETRA_TYPE TetraQ 
+#define TETRA_TYPE TetraTQ 
 
-#define __VCGLIB_TETRA_Q
+#define __VCGLIB_TETRA_TQ
 
 #include <vcg/simplex/tetrahedron/base.h> 
 
 #undef TETRA_TYPE 
 
-#undef __VCGLIB_TETRA_Q
+#undef __VCGLIB_TETRA_TQ
 
 #endif
