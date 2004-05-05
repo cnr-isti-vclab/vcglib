152e3b1022207590e6b95288eadedbda1dd0ab17
nicopietroni <nico.pietroni@isti.cnr.it>
2004-05-05 12:23:25 +0000

    with normal

---
+++ vcg/simplex/tetrahedron/with/tn.h
@@ -0,0 +1,14 @@
+#ifndef __VCGLIB_TETRA_TN_TYPE
+#define __VCGLIB_TETRA_TN_TYPE
+
+#define TETRA_TYPE TetraTN 
+
+#define __VCGLIB_TETRA_TN
+
+#include <vcg/simplex/tetrahedron/base.h> 
+
+#undef TETRA_TYPE 
+
+#undef __VCGLIB_TETRA_TN
+
+#endif
