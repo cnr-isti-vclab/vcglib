bdb47046da0ab1f94d79a80382dbc0b7fd8f7a28
nicopietroni <nico.pietroni@isti.cnr.it>
2004-05-05 16:31:47 +0000

    with normal and vertex-face topology

---
+++ vcg/simplex/face/with/fnav.h
@@ -0,0 +1,16 @@
+#ifndef __VCGLIB_FACE_FNAV_TYPE
+#define __VCGLIB_FACE_FNAV_TYPE
+
+#define FACE_TYPE FaceFNAV 
+
+#define __VCGLIB_FACE_FN
+#define __VCGLIB_FACE_AV
+
+#include <vcg/simplex/face/base.h> 
+
+#undef FACE_TYPE 
+
+#undef __VCGLIB_FACE_FN
+#undef __VCGLIB_FACE_AV
+
+#endif
