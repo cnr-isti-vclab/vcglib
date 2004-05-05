286971f8ae24a2ad20b1cb8a65b7b4570face36e
nicopietroni <nico.pietroni@isti.cnr.it>
2004-05-05 16:29:33 +0000

    with face topology

---
+++ vcg/simplex/face/with/af.h
@@ -0,0 +1,14 @@
+#ifndef __VCGLIB_FACE_AF_TYPE
+#define __VCGLIB_FACE_AF_TYPE
+
+#define FACE_TYPE FaceAF 
+
+#define __VCGLIB_FACE_AF
+
+#include <vcg/simplex/face/base.h> 
+
+#undef FACE_TYPE 
+
+#undef __VCGLIB_FACE_AF
+
+#endif
