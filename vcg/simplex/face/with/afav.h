8d49b3d7f51c6f1c4fbc64a3f9ba9f3e85ae9fdc
nicopietroni <nico.pietroni@isti.cnr.it>
2004-05-06 14:58:00 +0000

    face with both topology ( vertex -face) (face-face)

---
+++ vcg/simplex/face/with/afav.h
@@ -0,0 +1,16 @@
+#ifndef __VCGLIB_FACE_AFAV_TYPE
+#define __VCGLIB_FACE_AFAV_TYPE
+
+#define FACE_TYPE FaceAFAV 
+
+#define __VCGLIB_FACE_AV
+#define __VCGLIB_FACE_AF
+
+#include <vcg/simplex/face/base.h> 
+
+#undef FACE_TYPE 
+
+#undef __VCGLIB_FACE_AF
+#undef __VCGLIB_FACE_AV
+
+#endif
