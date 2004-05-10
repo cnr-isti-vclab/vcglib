d7d6388dbfdc1987e7a61f55eed86d0daef14c39
ganovelli <fabio.ganovelli@isti.cnr.it>
2004-05-10 14:45:46 +0000

    created

---
+++ vcg/simplex/edge/with/ae.h
@@ -0,0 +1,14 @@
+#ifndef __VCGLIB_EDGE_AE_TYPE
+#define __VCGLIB_EDGE_AE_TYPE
+
+#define EDGE_TYPE EdgeAE 
+
+#define __VCGLIB_EDGE_AE
+
+#include <vcg/simplex/edge/base.h> 
+
+#undef EDGE_TYPE 
+
+#undef __VCGLIB_EDGE_AE
+
+#endif
