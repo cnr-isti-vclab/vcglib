a989a39c199dcca32f7ba4423b9ddcbaea840cc8
ganovelli <fabio.ganovelli@isti.cnr.it>
2004-05-11 15:20:30 +0000

    *** empty log message ***

--- vcg/simplex/face/with/avfn.h
+++ vcg/simplex/face/with/avfn.h
@@ -1,7 +1,7 @@
-#ifndef __VCGLIB_FACE_FNAV_TYPE
-#define __VCGLIB_FACE_FNAV_TYPE
+#ifndef __VCGLIB_FACE_AVFN_TYPE
+#define __VCGLIB_FACE_AVFN_TYPE
 
-#define FACE_TYPE FaceFNAV 
+#define FACE_TYPE FaceAVFN 
 
 #define __VCGLIB_FACE_FN
 #define __VCGLIB_FACE_AV
