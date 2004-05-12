f9eae452b7521b43d1d7643f878a0a7e405bd715
cignoni <paolo.cignoni@isti.cnr.it>
2004-05-12 10:31:34 +0000

    Initial Commit

---
+++ vcg/simplex/vertex/with/vcvmvnvq.h
@@ -0,0 +1,27 @@
+#ifndef __VCGLIB_VERTEX__VCVN__TYPE
+#define __VCGLIB_VERTEX__VCVN__TYPE
+
+
+#define VERTEX_TYPE VertexVCVMVNVQ 
+
+#define __VCGLIB_VERTEX_VC
+#define __VCGLIB_VERTEX_VM
+#define __VCGLIB_VERTEX_VN
+#define __VCGLIB_VERTEX_VQ
+
+#include <vcg/simplex/vertex/base.h> 
+
+
+#undef VERTEX_TYPE 
+#undef __VCGLIB_VERTEX_VC
+#undef __VCGLIB_VERTEX_VM
+#undef __VCGLIB_VERTEX_VN
+#undef __VCGLIB_VERTEX_VQ
+
+
+namespace vcg {
+typedef VertexVCVMVNVQ<float>  VertexVCVNVMVQf;
+typedef VertexVCVMVNVQ<double> VertexVCVNVMVQd;
+}
+
+#endif
