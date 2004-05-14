a1722b063f6f9ee7093c82333f9fdeb583feddf0
ganovelli <fabio.ganovelli@isti.cnr.it>
2004-05-14 00:32:36 +0000

    just color and quality on the vertex

--- apps/metro/mesh_type.h
+++ apps/metro/mesh_type.h
@@ -1,39 +1,40 @@
-/*****************************************************************************
- * VCGLib                                                                    *
- *																																					 *
- * Visual Computing Group                                                o>  *
- * IEI Institute, CNUCE Institute, CNR Pisa                             <|   *
- *                                                                      / \  *
- * Copyright(C) 1999 by Paolo Cignoni, Claudio Rocchini                      *
- * All rights reserved.                                                      *
- *																																					 *
- * Permission  to use, copy, modify, distribute  and sell this  software and *
- * its documentation for any purpose is hereby granted without fee, provided *
- * that  the above copyright notice appear  in all copies and that both that *
- * copyright   notice  and  this  permission  notice  appear  in  supporting *
- * documentation. the author makes  no representations about the suitability *
- * of this software for any purpose. It is provided  "as is" without express *
- * or implied warranty.                                                      *
- *					                                                         *
- *****************************************************************************/
+/****************************************************************************
+* VCGLib                                                            o o     *
+* Visual and Computer Graphics Library                            o     o   *
+*                                                                _   O  _   *
+* Copyright(C) 2004                                                \/)\/    *
+* Visual Computing Lab                                            /\/|      *
+* ISTI - Italian National Research Council                           |      *
+*                                                                    \      *
+* All rights reserved.                                                      *
+*                                                                           *
+* This program is free software; you can redistribute it and/or modify      *   
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 2 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+*                                                                           *
+****************************************************************************/
 /****************************************************************************
   History
-	2003 Dic 17 modifiche per conversione alla versione template	(gano)
-****************************************************************************/
 
-// -----------------------------------------------------------------------------------------------
+$Log: not supported by cvs2svn $
+
+
+****************************************************************************/
 #ifndef _CMESH_H
 #define _CMESH_H
-// -----------------------------------------------------------------------------------------------
 
 #pragma warning(disable:4786 4804 4666) 
-// standard libraries.
-#include <math.h>
 
-// VCG library.
-//#include <vcg/tools/plylib.h>
-#include <vcg/simplex/vertex/with/vcvmvnvq.h>
-#include <vcg/simplex/face/with/rtfmfn.h>
+#include <math.h>
+#include <vcg/simplex/vertex/with/vcvq.h>
+#include <vcg/simplex/face/with/rtfcfmfn.h>
 #include <vcg/space/index/grid_static_ptr.h>
 #include <vcg/complex/trimesh/base.h>
 
@@ -45,10 +46,8 @@ using namespace std;
 // Vertex, Face, Mesh and Grid definitions.
 class MyEdge;
 class CFace;
-class CVertex   : public VertexVCVMVNVQ< double,MyEdge,CFace > {};
-class CFace     : public FaceRTFMFN< CVertex,MyEdge,CFace > {};
+class CVertex   : public VertexVCVQ< double,MyEdge,CFace > {};
+class CFace     : public FaceRTFCFMFN< CVertex,MyEdge,CFace > {};
 class CMesh     : public tri::TriMesh< vector<CVertex>, vector<CFace> > {};
 
-// -----------------------------------------------------------------------------------------------
 #endif
-// -----------------------------------------------------------------------------------------------
