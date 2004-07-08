2542c1654afdef55e79f38d4a4c1fde8c20a3f37
ganovelli <fabio.ganovelli@isti.cnr.it>
2004-07-08 13:20:10 +0000

    renamed to comply  alphabetical order

---
+++ vcg/simplex/vertex/with/atvmvn.h
@@ -0,0 +1,25 @@
+#ifndef __VCGLIB_VERTEX__AT__TYPE
+#define __VCGLIB_VERTEX__AT__TYPE
+
+#define VERTEX_TYPE VertexATVN
+
+#define __VCGLIB_VERTEX_AT
+#define __VCGLIB_VERTEX_VN
+
+#include <vcg/simplex/vertex/base.h> 
+
+#undef VERTEX_TYPE 
+#undef __VCGLIB_VERTEX_AT
+#undef __VCGLIB_VERTEX_VN
+
+namespace vcg {
+
+template < class VETYPE, class VFTYPE, class VTTYPE>
+class VertexATVNf : public VertexATVN<float,VETYPE,VFTYPE,VTTYPE> {};
+
+template < class VETYPE, class VFTYPE, class VTTYPE>
+class VertexATVNd : public VertexATVN<double,VETYPE,VFTYPE,VTTYPE> {};
+
+}
+
+#endif
