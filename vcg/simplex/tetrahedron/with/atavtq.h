ae23bd9a4bc5c715242e9829f1e0bf136387fad6
ganovelli <fabio.ganovelli@isti.cnr.it>
2004-07-08 09:02:27 +0000

    tv-->av

--- vcg/simplex/tetrahedron/with/atavtq.h
+++ vcg/simplex/tetrahedron/with/atavtq.h
@@ -4,14 +4,14 @@
 #define TETRA_TYPE TetraATAVTQ 
 
 #define __VCGLIB_TETRA_AT
-#define __VCGLIB_TETRA_VT
+#define __VCGLIB_TETRA_AV
 #define __VCGLIB_TETRA_TQ
 #include <vcg/simplex/tetrahedron/base.h> 
 
 #undef TETRA_TYPE 
 
 #undef __VCGLIB_TETRA_AT
-#undef __VCGLIB_TETRA_VT
+#undef __VCGLIB_TETRA_AV
 #undef __VCGLIB_TETRA_TQ
 
 #endif
