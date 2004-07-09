24583cbed20d0f0cef85923cd1390baf5ef22896
ganovelli <fabio.ganovelli@isti.cnr.it>
2004-07-09 10:10:22 +0000

    muted from atvnvm

--- vcg/simplex/vertex/with/atvmvn.h
+++ vcg/simplex/vertex/with/atvmvn.h
@@ -1,24 +1,26 @@
 #ifndef __VCGLIB_VERTEX__AT__TYPE
 #define __VCGLIB_VERTEX__AT__TYPE
 
-#define VERTEX_TYPE VertexATVN
+#define VERTEX_TYPE VertexATVMVN
 
 #define __VCGLIB_VERTEX_AT
+#define __VCGLIB_VERTEX_VM
 #define __VCGLIB_VERTEX_VN
 
 #include <vcg/simplex/vertex/base.h> 
 
 #undef VERTEX_TYPE 
 #undef __VCGLIB_VERTEX_AT
+#undef __VCGLIB_VERTEX_VM
 #undef __VCGLIB_VERTEX_VN
 
 namespace vcg {
 
 template < class VETYPE, class VFTYPE, class VTTYPE>
-class VertexATVNf : public VertexATVN<float,VETYPE,VFTYPE,VTTYPE> {};
+class VertexATVMVNf : public VertexATVMVN<float,VETYPE,VFTYPE,VTTYPE> {};
 
 template < class VETYPE, class VFTYPE, class VTTYPE>
-class VertexATVNd : public VertexATVN<double,VETYPE,VFTYPE,VTTYPE> {};
+class VertexATVMVNd : public VertexATVMVN<double,VETYPE,VFTYPE,VTTYPE> {};
 
 }
 
