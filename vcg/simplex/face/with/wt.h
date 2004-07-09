1f3fc8f9ce070daaf844c39c53eacddf6023c8dc
mtarini <marco.tarini@isti.cnr.it>
2004-07-09 15:52:04 +0000

    *** empty log message ***

---
+++ vcg/simplex/face/with/wt.h
@@ -0,0 +1,27 @@
+#ifndef __VCGLIB_FACE_WT_TYPE
+
+#define __VCGLIB_FACE_WT_TYPE
+
+
+
+#define FACE_TYPE FaceWT 
+
+
+
+#define __VCGLIB_FACE_WT
+
+
+
+#include <vcg/simplex/face/base.h> 
+
+
+
+#undef FACE_TYPE 
+
+
+
+#undef __VCGLIB_FACE_WT
+
+
+
+#endif
