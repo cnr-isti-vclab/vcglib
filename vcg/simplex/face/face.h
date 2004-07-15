01f33c04ceb095bec4b5c2299fa261fdd7967d5a
ganovelli <fabio.ganovelli@isti.cnr.it>
2004-07-15 11:31:06 +0000

    overwritten previous version (it was a copy of base.h)

--- vcg/simplex/face/face.h
+++ vcg/simplex/face/face.h
@@ -1,10 +1,38 @@
-#ifndef __VCGLIB_FACE_TYPE
-#define __VCGLIB_FACE_TYPE
+/****************************************************************************
+* VCGLib                                                            o o     *
+* Visual and Computer Graphics Library                            o     o   *
+*                                                                _   O  _   *
+* Copyright(C) 2004                                                \/)\/    *
+* Visual Computing Lab                                            /\/|      *
+* ISTI - Italian National Research Council                           |      *
+*                                                                    \      *
+* All rights reserved.                                                      *
+*                                                                           *
+* This program is free software; you can redistribute it and/or modify      *   
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 2 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+*                                                                           *
+****************************************************************************/
+/****************************************************************************
+  History
+
+$Log: not supported by cvs2svn $
+First commit...
+
+****************************************************************************/
+
+#ifndef __VCGLIB__BASICFACETYPE
+#define __VCGLIB__BASICFACETYPE
 
 #define FACE_TYPE Face
+#include <vcg/simplex/face/base.h>
 
-#include <vcg/simplex/face/base.h> 
-
-#undef FACE_TYPE 
-
+#undef FAce_TYPE
 #endif
