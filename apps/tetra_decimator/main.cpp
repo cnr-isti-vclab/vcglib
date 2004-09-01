c3227ae7a0d248ba9f6a774192bbda617fe91efa
nicopietroni <nico.pietroni@isti.cnr.it>
2004-09-01 12:08:39 +0000

    minor changes

--- apps/tetra_decimator/main.cpp
+++ apps/tetra_decimator/main.cpp
@@ -7,9 +7,6 @@
 #include <vcg/complex/tetramesh/base.h>
 #include <vcg/simplex/tetrahedron/with/atavtq.h>
 
-// the trackball
-#include <wrap/gui/trackball.h>
-
 // io
 #include <wrap/io_tetramesh/import_ply.h>
 #include <wrap/io_tetramesh/export_ply.h>
@@ -25,28 +22,25 @@ class MyTetrahedron : public vcg::TetraATAVTQ<MyVertex,MyTetrahedron>{};
 class MyTMesh: public vcg::tetra::Tetramesh< std::vector<MyVertex>, std::vector<MyTetrahedron > >{};
 
 
-#include <vcg/complex/local_optimization/base.h>
+#include <vcg/complex/local_optimization.h>
 #include <vcg/complex/local_optimization/tetra_edge_collapse.h>
 
 
-vcg::LocalOptimization<MyTMesh> loc;
-vcg::tetra::TetraEdgeCollapse<MyTMesh> c;
+vcg::LocalOptimization<MyTMesh> *loc;
+//vcg::tetra::TetraEdgeCollapse<MyTMesh> c;
 MyTMesh mesh;
 
-int main(int,char**argv){
-
-	loc.m = & mesh;
+int main(int,char**argv,int num_op){
+	loc=new vcg::LocalOptimization<MyTMesh>(mesh);
 
 	vcg::tetra::io::ImporterTS<MyTMesh>::Open(mesh,argv[1]);
+	//vcg::tetra::io::ImporterTS<MyTMesh>::Open(mesh,"d:/sphere.ts");
 	printf("mesh loaded %d %d \n",mesh.vn,mesh.tn);
-//	vcg::tetra::io::ExporterPLY<MyTMesh>::Save(mesh,(string(argv[1])+string(".ply")).c_str());
-	
 
+	/*vcg::tetra::TetraEdgeCollapse<MyTMesh> *_ ;
+	bool res;*/
 
-	vcg::tetra::TetraEdgeCollapse<MyTMesh> *_ ;
-	bool res;
-	do{
-			vcg::tetra::UpdateTetraTopology<MyTMesh::VertexContainer,MyTMesh::TetraContainer>
+	vcg::tetra::UpdateTetraTopology<MyTMesh::VertexContainer,MyTMesh::TetraContainer>
 		::VTTopology(mesh.vert,mesh.tetra);
 
 	vcg::tetra::UpdateTetraTopology<MyTMesh::VertexContainer,MyTMesh::TetraContainer>
@@ -55,13 +49,13 @@ int main(int,char**argv){
 	vcg::tetra::UpdateTetraTopology<MyTMesh::VertexContainer,MyTMesh::TetraContainer>
 		::setExternalVertices(mesh.vert,mesh.tetra);
 
-	 _=new vcg::tetra::TetraEdgeCollapse<MyTMesh>();
-		loc.h.push_back(vcg::LocalOptimization<MyTMesh>::HeapElem(_));
-
-		loc.Init();
-		loc.SetTargetSimplices(10);
+	vcg::tetra::TetraEdgeCollapse<MyTMesh>::Init(mesh,loc->h);
+	
+	bool res;
+	do{
+		loc->SetTargetOperations(num_op);
 		
-		 res = loc.DoOptimization();
+		res = loc->DoOptimization();
 
 		printf("ood %d\n bor %d\n vol %d \n lkv %d \n lke %d \n lkf %d \n",
 						FAIL::OFD(),
@@ -74,7 +68,7 @@ int main(int,char**argv){
 		printf("mesh  %d %d \n",mesh.vn,mesh.tn);
 	}while(!res);
 
-
+	
 	vcg::tetra::io::ExporterPLY<MyTMesh>::Save(mesh,"out.ply");
 	
 	return 0;
