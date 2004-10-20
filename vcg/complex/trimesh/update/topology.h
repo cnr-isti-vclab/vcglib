f456dc361c76372abeed03b2d10dca685ed58248
cignoni <paolo.cignoni@isti.cnr.it>
2004-10-20 07:33:10 +0000

    removed FaceBorderFlags (already present in update/flags.h)

--- vcg/complex/trimesh/update/topology.h
+++ vcg/complex/trimesh/update/topology.h
@@ -24,6 +24,9 @@
   History
 
 $Log: not supported by cvs2svn $
+Revision 1.14  2004/10/18 17:10:22  ganovelli
+added  ::FaceBorderFLags
+
 Revision 1.13  2004/10/01 15:58:00  ponchio
 Added include <vector>
 
@@ -212,18 +215,6 @@ static void FaceFace(MeshType &m)
 	}
 }
 
-/** Update the Faces' border flag (it assumes Face-Face Topology has been computed.
-*/
-static void FaceBorderFlags(MeshType &m)
-{
-		FaceIterator pf;
-		for(pf=m.face.begin();pf!=m.face.end();++pf)			// Lo riempio con i dati delle facce
-			if( ! (*pf).IsD() )
-				for(int j=0;j<3;++j)
-					if((*pf).IsBorder(j))
-						(*pf).SetB(j);
-
-}
 
 /** Update the Vertex-Face topological relation by allowing to retrieve for each vertex the list of faces sharing this vertex..
 */
