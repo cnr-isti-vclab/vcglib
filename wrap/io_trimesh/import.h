aee55d8418f5cc9cbad2b6d5afe52a21576edce9
cignoni <paolo.cignoni@isti.cnr.it>
2004-11-29 08:12:10 +0000

    Initial Update

---
+++ wrap/io_trimesh/import.h
@@ -0,0 +1,114 @@
+/****************************************************************************
+* VCGLib                                                            o o     *
+* Visual and Computer Graphics Library                            o     o   *
+*                                                                _   O  _   *
+* Copyright(C) 2004                                                \/)\/    *
+* Visual Computing Lab                                            /\/|      *
+* ISTI - Italian National Research Council                           |      *
+*                                                                    \      *
+* All rights reserved.                                                      *
+*                                                                           *
+* This program is free software; you can redistribute it and/or modify      *   
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 2 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+*                                                                           *
+****************************************************************************/
+
+/****************************************************************************
+  History
+
+$Log: not supported by cvs2svn $
+
+****************************************************************************/
+
+#ifndef __VCGLIB_IMPORT
+#define __VCGLIB_IMPORT
+
+#include <wrap/io_trimesh/import_ply.h>
+#include <wrap/io_trimesh/import_stl.h>
+#include <wrap/io_trimesh/import_off.h>
+
+#include <locale>
+
+namespace vcg {
+namespace tri {
+namespace io {
+
+/** 
+This class encapsulate a filter for automatically importing meshes by guessing 
+the right filter according to the extension
+*/
+
+template <class OpenMeshType>
+class Importer
+{
+private:
+  enum KnownTypes { KT_UNKNOWN, KT_PLY, KT_STL, KT_OFF };
+static int &LastType()
+{
+  static int lastType= KT_UNKNOWN;
+return lastType;
+}
+
+public:
+// simple aux function that returns true if a given file has a given extesnion
+static bool FileExtension(string filename,  string extension)
+{
+  locale loc1 ;
+  use_facet<ctype<char> > ( loc1 ).tolower(&*filename.begin(),&*filename.end());
+  use_facet<ctype<char> > ( loc1 ).tolower(&*extension.begin(),&*extension.end());
+  string end=filename.substr(filename.length()-extension.length(),extension.length());
+  return end==extension;
+}
+
+// Open Mesh
+static int Open(OpenMeshType &m, const char *filename, CallBackPos *cb=0)
+{
+  int err;
+  if(FileExtension(filename,"ply"))
+  {
+    err = ImporterPLY<OpenMeshType>::Open(m,filename);
+    LastType()=KT_PLY;
+  }
+  else if(FileExtension(filename,"stl"))
+  {
+    err = ImporterSTL<OpenMeshType>::Open(m,filename);
+    LastType()=KT_STL;
+  }
+   else if(FileExtension(filename,"off"))
+  {
+    err = ImporterOFF<OpenMeshType>::Open(m,filename);
+    LastType()=KT_OFF;
+  }
+ else {
+    err=1;
+    LastType()=KT_UNKNOWN;
+  }
+
+  return err;
+}
+
+static const char *ErrorMsg(int error)
+{
+  switch(LastType())
+  {
+    case KT_PLY : return ImporterPLY<OpenMeshType>::ErrorMsg(error); break;
+    case KT_STL : return ImporterSTL<OpenMeshType>::ErrorMsg(error); break;
+    case KT_OFF : return ImporterOFF<OpenMeshType>::ErrorMsg(error); break;
+  }
+  return "Unknown type";  
+}
+
+}; // end class
+} // end Namespace tri
+} // end Namespace io
+} // end Namespace vcg
+
+#endif
