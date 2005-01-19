29e46cb2af5a925cff3fa13c7ed4d571f6ec8abe
cnr-isti-vclab <paolo.cignoni@isti.cnr.it>
2005-01-19 15:37:11 +0000

    simple project that make use of the MC and EMC algorithms

---
+++ apps/test/extractors/extractor/main.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <wrap/io_trimesh/export_ply.h>
+#include "Definitions.h"
+#include "Volume.h"
+#include "Walker.h"
+#include <vcg/complex/trimesh/create/marching_cubes.h>
+#include <vcg/complex/trimesh/create/extended_marching_cubes.h>
+
+int main(int argc, char *argv[])
+{
+	BoundingBox	bbox(vcg::Point3i(-20, -20, -20), vcg::Point3i(20, 20, 20));
+	vcg::Point3i			resolution(40, 40, 40);
+	
+	Volume	volume;
+	Walker	walker(bbox, resolution);
+
+	typedef vcg::tri::MarchingCubes<Mesh, Walker>					MarchingCubes;
+	typedef vcg::tri::ExtendedMarchingCubes<Mesh, Walker> ExtendedMarchingCubes;
+
+	
+	
+	// MARCHING CUBES
+	Mesh		mc_mesh;
+	MarchingCubes					mc(mc_mesh, walker);
+	walker.BuildMesh<MarchingCubes>(mc_mesh, volume, mc);
+	vcg::tri::io::ExporterPLY<Mesh>::Save( mc_mesh, "marching_cubes.ply");
+
+	// EXTENDED MARCHING CUBES
+	Mesh		emc_mesh;
+	ExtendedMarchingCubes emc(emc_mesh, walker, 30);
+	walker.BuildMesh<ExtendedMarchingCubes>(emc_mesh, volume, emc);
+	vcg::tri::io::ExporterPLY<Mesh>::Save( emc_mesh, "extended_marching_cubes.ply");
+};
