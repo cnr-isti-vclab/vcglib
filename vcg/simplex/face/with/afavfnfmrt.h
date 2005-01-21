a264ec7c786be64b5d50989a03cc3cc61347eac7
cnr-isti-vclab <paolo.cignoni@isti.cnr.it>
2005-01-21 16:42:53 +0000

    #define __VCGLIB_FACE_AF #define __VCGLIB_FACE_FN #define __VCGLIB_FACE_AV #define __VCGLIB_FACE_FM #define __VCGLIB_FACE_RT

---
+++ vcg/simplex/face/with/afavfnfmrt.h
@@ -0,0 +1,21 @@
+#ifndef __VCGLIB_FACE_AFAVFNFMRT_TYPE
+#define __VCGLIB_FACE_AFAVFNFMRT_TYPE
+
+#define FACE_TYPE FaceAFAVFNFMRT
+
+#define __VCGLIB_FACE_AF
+#define __VCGLIB_FACE_FN
+#define __VCGLIB_FACE_AV
+#define __VCGLIB_FACE_FM
+#define __VCGLIB_FACE_RT
+
+#include <vcg/simplex/face/base.h> 
+
+#undef FACE_TYPE 
+
+#undef __VCGLIB_FACE_AF
+#undef __VCGLIB_FACE_FN
+#undef __VCGLIB_FACE_AV
+#undef __VCGLIB_FACE_FM
+#undef __VCGLIB_FACE_RT
+#endif
