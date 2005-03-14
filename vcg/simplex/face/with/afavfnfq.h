6063b7526d4bb22133e5774293d2c9eb4a7968cb
ganovelli <fabio.ganovelli@isti.cnr.it>
2005-03-14 16:36:59 +0000

    *** empty log message ***

---
+++ vcg/simplex/face/with/afavfnfq.h
@@ -0,0 +1,19 @@
+#ifndef __VCGLIB_FACE_AFAVFNFQ_TYPE
+#define __VCGLIB_FACE_AFAVFNFQ_TYPE
+
+#define FACE_TYPE FaceAFAVFNFQ
+
+#define __VCGLIB_FACE_AF
+#define __VCGLIB_FACE_FN
+#define __VCGLIB_FACE_AV
+#define __VCGLIB_FACE_FQ
+
+#include <vcg/simplex/face/base.h> 
+
+#undef FACE_TYPE 
+
+#undef __VCGLIB_FACE_AF
+#undef __VCGLIB_FACE_FN
+#undef __VCGLIB_FACE_AV
+#undef __VCGLIB_FACE_FQ
+#endif
