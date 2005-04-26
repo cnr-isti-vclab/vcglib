966e1e52f0824b36e419edda2c7cd181bbe66011
nicopietroni <nico.pietroni@isti.cnr.it>
2005-04-26 09:36:17 +0000

    *** empty log message ***

---
+++ vcg/simplex/face/with/affnfmrt.h
@@ -0,0 +1,19 @@
+#ifndef __VCGLIB_FACE_AFAVFNFMRT_TYPE
+#define __VCGLIB_FACE_AFAVFNFMRT_TYPE
+
+#define FACE_TYPE FaceAFFNFMRT
+
+#define __VCGLIB_FACE_AF
+#define __VCGLIB_FACE_FN
+#define __VCGLIB_FACE_FM
+#define __VCGLIB_FACE_RT
+
+#include <vcg/simplex/face/base.h> 
+
+#undef FACE_TYPE 
+
+#undef __VCGLIB_FACE_AF
+#undef __VCGLIB_FACE_FN
+#undef __VCGLIB_FACE_FM
+#undef __VCGLIB_FACE_RT
+#endif
