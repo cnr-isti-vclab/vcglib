b12f308b33f785f9b3777f3f68b17629810230c2
nicopietroni <nico.pietroni@isti.cnr.it>
2005-04-28 12:55:41 +0000

    *** empty log message ***

---
+++ vcg/simplex/face/with/affn.h
@@ -0,0 +1,17 @@
+#ifndef __VCGLIB_FACE_AFFN_TYPE
+#define __VCGLIB_FACE_AFFN_TYPE
+
+#define FACE_TYPE FaceAFFN
+
+#define __VCGLIB_FACE_AF
+#define __VCGLIB_FACE_FN
+
+
+#include <vcg/simplex/face/base.h> 
+
+#undef FACE_TYPE 
+
+#undef __VCGLIB_FACE_AF
+#undef __VCGLIB_FACE_FN
+
+#endif
