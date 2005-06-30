580e1eb075beff613b0af9e3ef8ac4dfb55e0b15
granzuglia <guido.ranzuglia@isti.cnr.it>
2005-06-30 15:46:24 +0000

    new tetra type

---
+++ vcg/simplex/tetrahedron/with/atavtn.h
@@ -0,0 +1,17 @@
+#ifndef __VCGLIB_TETRA_ATTN_TYPE
+#define __VCGLIB_TETRA_ATTN_TYPE
+
+#define TETRA_TYPE TetraATAVTN
+
+#define __VCGLIB_TETRA_AT
+#define __VCGLIB_TETRA_AV
+#define __VCGLIB_TETRA_TN
+#include <vcg/simplex/tetrahedron/base.h> 
+
+#undef TETRA_TYPE 
+
+#undef __VCGLIB_TETRA_AT
+#undef __VCGLIB_TETRA_AV
+#undef __VCGLIB_TETRA_TN
+
+#endif
---
+++ vcg/simplex/tetrahedron/with/attn.h
@@ -0,0 +1,15 @@
+#ifndef __VCGLIB_TETRA_ATTN_TYPE
+#define __VCGLIB_TETRA_ATTN_TYPE
+
+#define TETRA_TYPE TetraATTN
+
+#define __VCGLIB_TETRA_AT
+#define __VCGLIB_TETRA_TN
+#include <vcg/simplex/tetrahedron/base.h> 
+
+#undef TETRA_TYPE 
+
+#undef __VCGLIB_TETRA_AT
+#undef __VCGLIB_TETRA_TN
+
+#endif
