38db49c82744357343792476372ed08efd3e6d89
cignoni <paolo.cignoni@isti.cnr.it>
2005-07-05 11:26:09 +0000

    first version

---
+++ vcg/simplex/face/with/affm.h
@@ -0,0 +1,16 @@
+#ifndef __VCGLIB_FACE_AFFM_TYPE
+#define __VCGLIB_FACE_AFFM_TYPE
+
+#define FACE_TYPE FaceAFFM 
+
+#define __VCGLIB_FACE_AF
+#define __VCGLIB_FACE_FM
+
+#include <vcg/simplex/face/base.h> 
+
+#undef FACE_TYPE 
+
+#undef __VCGLIB_FACE_AF
+#undef __VCGLIB_FACE_FM
+
+#endif
