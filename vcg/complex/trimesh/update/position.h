57c08ade0f3e61e77ddcc216402db567d8eceec9
cignoni <paolo.cignoni@isti.cnr.it>
2005-07-06 08:02:27 +0000

    Initial commit

---
+++ vcg/complex/trimesh/update/position.h
@@ -0,0 +1,68 @@
+/****************************************************************************
+* VCGLib                                                            o o     *
+* Visual and Computer Graphics Library                            o     o   *
+*                                                                _   O  _   *
+* Copyright(C) 2004                                                \/)\/    *
+* Visual Computing Lab                                            /\/|      *
+* ISTI - Italian National Research Council                           |      *
+*                                                                    \      *
+* All rights reserved.                                                      *
+*                                                                           *
+* This program is free software; you can redistribute it and/or modify      *   
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 2 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+*                                                                           *
+****************************************************************************/
+/****************************************************************************
+  History
+
+$Log: not supported by cvs2svn $
+
+****************************************************************************/
+#ifndef __VCG_TRI_UPDATE_POSITION
+#define __VCG_TRI_UPDATE_POSITION
+
+namespace vcg {
+namespace tri {
+
+/** \addtogroup trimesh */
+/*@{*/
+
+/// This class is used to update vertex position according to a transformation matrix.
+template <class ComputeMeshType>
+class UpdatePosition
+{
+
+public:
+typedef ComputeMeshType MeshType; 
+typedef typename MeshType::ScalarType     ScalarType;
+typedef typename MeshType::VertexType     VertexType;
+typedef typename MeshType::VertexPointer  VertexPointer;
+typedef typename MeshType::VertexIterator VertexIterator;
+typedef typename MeshType::FaceType       FaceType;
+typedef typename MeshType::FacePointer    FacePointer;
+typedef typename MeshType::FaceIterator   FaceIterator;
+
+/// Multiply 
+static void Matrix(ComputeMeshType &m, const Matrix44<ScalarType> &M)
+{
+	VertexIterator vi;
+	for(vi=m.vert.begin();vi!=m.vert.end();++vi)
+	        if(!(*vi).IsD()) (*vi).P()=M*(*vi).cP();
+}
+
+
+}; // end class
+
+}	// End namespace
+}	// End namespace
+
+
+#endif
