de125d7f597ad3217729ec35e2d05fcbccdb02d2
nicopietroni <nico.pietroni@isti.cnr.it>
2005-10-03 14:13:13 +0000

    first release version

---
+++ vcg/simplex/vertex/distance.h
@@ -0,0 +1,58 @@
+/****************************************************************************
+* VCGLib                                                            o o     *
+* Visual and Computer Graphics Library                            o     o   *
+*                                                                _   O  _   *
+* Copyright(C) 2004                                                \/)\/    *
+* Visual Computing Lab                                            /\/|      *
+* ISTI - Italian National Research Council                           |      *
+*                                                                    \      *
+* All rights reserved.                                                      *
+*                                                                           *
+* This program is free software; you can redistribute it and/or modify      *   
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 2 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+*                                                                           *
+****************************************************************************/
+/****************************************************************************
+  History
+
+****************************************************************************/
+
+#ifndef __VCGLIB_VERTEX_DISTANCE
+#define __VCGLIB_VERTEX_DISTANCE
+
+#include <vcg/math/base.h>
+#include <vcg/space/point3.h>
+
+
+namespace vcg {
+	namespace vertex{
+
+	class PointDistanceFunctor {
+	public:
+		template <class VERTEXTYPE, class SCALARTYPE>
+		inline bool operator () (const VERTEXTYPE & v, const Point3<SCALARTYPE> & p, SCALARTYPE & minDist, Point3<SCALARTYPE> & q) {
+			const Point3<typename VERTEXTYPE::ScalarType> fp = Point3<typename VERTEXTYPE::ScalarType>::Construct(p);
+			typename VERTEXTYPE::ScalarType md = (typename VERTEXTYPE::ScalarType)(minDist);
+			md=(v.P()-fp).Norm();
+			const bool ret = (md<minDist);
+			minDist = (SCALARTYPE)(md);
+			q = p;
+			return (ret);
+		}
+	};
+
+}	 // end namespace face
+	
+}	 // end namespace vcg
+
+
+#endif
+
