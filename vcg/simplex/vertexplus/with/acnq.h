69123d036e6e3ded721ebf4ba2fd2177f602e4f4
cignoni <paolo.cignoni@isti.cnr.it>
2005-10-07 15:22:07 +0000

    updated to the new naming scheme

--- vcg/simplex/vertexplus/with/acnq.h
+++
@@ -1,9 +0,0 @@
-#ifndef __VCGLIB_VERTEX_ACNQ_TYPE
-#define __VCGLIB_VERTEX_ACNQ_TYPE
-namespace vcg {
-  template <class FaceTemplate> 
-  class VertexACNQd  : public VertexSimp2< VertexACNQd, DumET, FaceTemplate, vert::Normal3d, vert::VFAdj, vert::Flag, vert::Coord3d> {};
-  template <class FaceTemplate> 
-  class VertexACNQf  : public VertexSimp2< VertexACNQf, DumET, FaceTemplate, vert::Normal3f, vert::VFAdj, vert::Flag, vert::Coord3f> {};
-}
-#endif
--- vcg/simplex/vertexplus/with/n.h
+++
@@ -1,9 +0,0 @@
-#ifndef __VCGLIB_VERTEX__N__TYPE
-#define __VCGLIB_VERTEX__N__TYPE
-namespace vcg {
-
-  class VertexNd  : public VertexSimp1< VertexNd, DumET, vert::Normal3d, vert::Flag, vert::Coord3d> {};
-  class VertexNf  : public VertexSimp1< VertexNf, DumET, vert::Normal3f, vert::Flag, vert::Coord3f> {};
-
-}
-#endif
