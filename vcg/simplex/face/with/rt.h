cc0e745c0939aad0107d9c4096b1750bce447191
ganovelli <fabio.ganovelli@isti.cnr.it>
2005-11-30 14:44:47 +0000

    creation

---
+++ vcg/simplex/face/with/rt.h
@@ -0,0 +1,14 @@
+#ifndef __VCGLIB_FACE_RT_TYPE
+#define __VCGLIB_FACE_RT_TYPE
+
+#define FACE_TYPE FaceRT
+
+
+#define __VCGLIB_FACE_RT
+
+#include <vcg/simplex/face/base.h> 
+
+#undef FACE_TYPE 
+
+#undef __VCGLIB_FACE_RT
+#endif
