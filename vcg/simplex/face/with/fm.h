3c222fd583ac9688b5d04b195120182240d52c1e
dibenedetto <marco.dibenedetto@isti.cnr.it>
2006-05-08 15:19:21 +0000

    First Commit.

---
+++ vcg/simplex/face/with/fm.h
@@ -0,0 +1,14 @@
+#ifndef __VCGLIB_FACE_FM_TYPE
+#define __VCGLIB_FACE_FM_TYPE
+
+#define FACE_TYPE FaceFM 
+
+#define __VCGLIB_FACE_FM
+
+#include <vcg/simplex/face/base.h> 
+
+#undef FACE_TYPE 
+
+#undef __VCGLIB_FACE_FM
+
+#endif /* __VCGLIB_FACE_FM_TYPE */
