5f842ad53e3e374459382f2f71539fa0b84e8f3b
nicopietroni <nico.pietroni@isti.cnr.it>
2006-07-26 08:13:57 +0000

    *** empty log message ***

---
+++ vcg/simplex/edge/with/emef.h
@@ -0,0 +1,14 @@
+#ifndef __VCGLIB_EDGE_EM_TYPE
+#define __VCGLIB_EDGE_EM_TYPE
+
+#define EDGE_TYPE EdgeEMEF 
+#define __VCGLIB_EDGE_EM
+#define __VCGLIB_EDGE_EF
+
+#include <vcg/simplex/edge/base.h> 
+
+#undef EDGE_TYPE 
+#undef __VCGLIB_EDGE_EF
+#undef __VCGLIB_EDGE_EM
+
+#endif
