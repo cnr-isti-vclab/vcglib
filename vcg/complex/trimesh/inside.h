f25f034dcb90ca35c4b8177bbf794e0b44b6c546
cnr-isti-vclab <paolo.cignoni@isti.cnr.it>
2007-06-07 15:34:11 +0000

    Minor changes.

--- vcg/complex/trimesh/inside.h
+++ vcg/complex/trimesh/inside.h
@@ -26,27 +26,38 @@
   History
 
 $Log: not supported by cvs2svn $
+Revision 1.3  2007/06/06 15:38:57  turini
+Use the barycenter function from triangle3.h instead of
+the one in face\base.h.
+
 Revision 1.2  2007/06/06 14:26:51  pietroni
 compiling error resolved
 
 
 
 ****************************************************************************/
+
+
+
 #include <vcg/space/ray3.h>
 #include <vcg/space/box3.h>
 #include <vcg/space/triangle3.h>
 
+
 #ifndef VCG_INSIDE
 #define VCG_INSIDE
 
-///this static funtion is used to see if one point is inside 
-///a triangular mesh or not... First parameter is a spatial indexing 
-///structure (eg. a grid) used to perform research operation, 
-///initialized with faces of the triangular mesh of type TriMeshType
+
+/// This static funtion is used to see if one point is inside a triangular mesh or not... 
+/// First parameter is a spatial indexing structure (eg. a grid) used to perform research operation, initialized with faces of the triangular mesh of type TriMeshType
+
 namespace vcg {
+	
 	namespace trimesh {
+		
 		template <class FaceSpatialIndexing,class TriMeshType>
-		class Inside{
+		class Inside
+		{
 
 		private:
 
@@ -55,59 +66,43 @@ namespace vcg {
 
 		public:
 
-
-
-			//return true if the point is inside the mesh 
-			static bool Is_Inside(TriMeshType &m,
-				FaceSpatialIndexing &_g_mesh,
-				const CoordType &test)
+			/// Return true if the point is inside the mesh.
+			static bool Is_Inside( TriMeshType & m, FaceSpatialIndexing & _g_mesh, const CoordType & test )
 			{
-
 				typedef typename TriMeshType::FaceType FaceType;
 				typedef typename TriMeshType::ScalarType ScalarType;
 				typedef typename TriMeshType::CoordType CoordType;
-
-				const ScalarType EPSILON=0.000001;
-
-				///first test if the element is inside the bb of the mesh
-				if (!m.bbox.IsIn(test))
-					return false;
+				const ScalarType EPSILON = 0.000001;
+				/// First test if the element is inside the bounding box of the mesh.
+				if( !( m.bbox.IsIn(test) ) ) return false;
 				else
 				{
 					ScalarType dist;
-					CoordType Norm,ip,nearest;
-					FaceType *f=vcg::trimesh::GetClosestFace<TriMeshType,FaceSpatialIndexing>(m,_g_mesh,test,m.bbox.Diag(),dist,nearest,Norm,ip);
-					//any face in the mesh
-					assert(f!=NULL);
-					//if the point is on the face is considered inside
-					if ((test-nearest).Norm()<=EPSILON)
-						return true;
-					///if the point is in surface of a face then is ok
-					if ((ip.V(0)>EPSILON)&&(ip.V(1)>EPSILON)&&(ip.V(2)>EPSILON))
+					CoordType Norm, ip, nearest;
+					FaceType *f = vcg::trimesh::GetClosestFace< TriMeshType, FaceSpatialIndexing >( m, _g_mesh, test, m.bbox.Diag(), dist, nearest, Norm, ip );
+					assert( f != NULL );			/// Check if there is any face in the mesh
+					/// If the point is on the face is considered inside.
+					if( ( test - nearest ).Norm() <= EPSILON ) return true;
+					/// Check if the closest point is inside a face
+					if( ( ip.V(0) > EPSILON ) && ( ip.V(1) > EPSILON ) && ( ip.V(2) > EPSILON ) )
 					{
-						if ((f->N()*(test-nearest))<0)///in this case normal direction is enought
-							return true;
-						else
-							return false;
+						/// Check if the test point is inside the mesh using the normal direction
+						vcg::Point3f debugn = f->N();
+						if( ( f->N() * ( test - nearest ) ) < 0 ) return true;
+						else return false;
 					}
-
-					///in this case we are not sure because
-					///hit an edge or a vertex then we use a ray that
-					///go until the barycenter of found face,
-					///then see normal value again
+					/// In this case we are not sure because hit an edge or a vertex.
+					/// So we use a ray that go until the barycenter of found face, then see normal value again
 					else
 					{
 						CoordType bary = vcg::Barycenter< FaceType >(*f);
-						vcg::Ray3<ScalarType> r;
-						r.Set(test,(bary-test));///set origin and direction
-						r.Normalize();
-						FaceType *f1=vcg::trimesh::DoRay<TriMeshType,FaceSpatialIndexing>(m,_g_mesh,r,m.bbox.Diag(),dist);
-						if (f1==NULL)
-							assert(0);
-						if ((f1->N()*(test-bary))<0)///in this case normal direction is enought
-							return true;
-						else
-							return false;
+						/// Set ray : origin and direction
+						vcg::Ray3<ScalarType> r; r.Set( test, ( bary - test ) ); r.Normalize();
+						FaceType *f1 = vcg::trimesh::DoRay< TriMeshType, FaceSpatialIndexing >( m, _g_mesh, r, m.bbox.Diag(), dist );
+						assert( f1 != NULL );
+						/// In this case normal direction is enough.
+						if( ( f1->N() * ( test - bary ) ) < 0 ) return true;
+						else return false;
 					}
 
 				}
