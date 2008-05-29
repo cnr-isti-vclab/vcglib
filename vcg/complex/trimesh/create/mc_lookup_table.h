9b324d2f50dd768531e15ff870cc1746a7172775
cignoni <paolo.cignoni@isti.cnr.it>
2008-05-29 20:54:34 +0000

    added missing license info

--- vcg/complex/trimesh/create/mc_lookup_table.h
+++ vcg/complex/trimesh/create/mc_lookup_table.h
@@ -6,6 +6,17 @@
 * @date    12/08/2002
 *
 * @brief   LookUpTable for the MarchingCubes 33 Algorithm
+*                                                                           
+* This program is free software; you can redistribute it and/or modify      
+* it under the terms of the GNU General Public License as published by      
+* the Free Software Foundation; either version 2 of the License, or         
+* (at your option) any later version.                                       
+*                                                                           
+* This program is distributed in the hope that it will be useful,           
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          
+* for more details.                                                         
 */
 //________________________________________________
 
