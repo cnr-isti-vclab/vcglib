86c16f82452038aa60096ca731a5f61911b1b5ab
cignoni <paolo.cignoni@isti.cnr.it>
2008-07-12 06:12:46 +0000

    Added missing license information

--- wrap/dae/colladaformat.h
+++ wrap/dae/colladaformat.h
@@ -1,3 +1,25 @@
+/****************************************************************************
+* VCGLib                                                            o o     *
+* Visual and Computer Graphics Library                            o     o   *
+*                                                                _   O  _   *
+* Copyright(C) 2004-2008                                           \/)\/    *
+* Visual Computing Lab                                            /\/|      *
+* ISTI - Italian National Research Council                           |      *
+*                                                                    \      *
+* All rights reserved.                                                      *
+*                                                                           *
+* This program is free software; you can redistribute it and/or modify      *   
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 2 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+*                                                                           *
+****************************************************************************/
 #ifndef _COLLADA_FORMAT_H
 #define _COLLADA_FORMAT_H
 
@@ -8,44 +30,10 @@
 
 
 template<typename POINTTYPE>
-struct CoordNumber
-{
-public:
-	static unsigned int coord()
-	{
-		return 0;
-	}
-};
-
-template<>
-struct CoordNumber<vcg::Point2f>
-{
-public:
-	static unsigned int coord() 
-	{
-		return 2;
-	}
-};
-
-template<>
-struct CoordNumber<vcg::Point3f>
-{
-public:
-	static unsigned int coord() 
-	{
-		return 3;
-	}
-};
-
-template<>
-struct CoordNumber<vcg::Point4f>
-{
-public:
-	static unsigned int coord() 
-	{
-		return 4;
-	}
-};
+           struct CoordNumber{public:	static unsigned int coord()	{		return 0; 	}};
+template<> struct CoordNumber<vcg::Point2f> { public:	static unsigned int coord() { return 2;	} };
+template<> struct CoordNumber<vcg::Point3f> { public:	static unsigned int coord() {	return 3;	} };
+template<> struct CoordNumber<vcg::Point4f> { public: static unsigned int coord() {	return 4;	} };
 
 
 namespace Collada
