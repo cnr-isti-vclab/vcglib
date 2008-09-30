7021c9af389286b2909a732251c19ade5127adfb
ganovelli <fabio.ganovelli@isti.cnr.it>
2008-09-30 10:35:44 +0000

    added warning for deprecation

--- vcg/simplex/vertex/base.h
+++ vcg/simplex/vertex/base.h
@@ -175,13 +175,13 @@ Edited Comments and GPL license
 
 ****************************************************************************/
 
-#pragma message("[VCGLIB Warning]  This way to define the simplex vertex is DEPRECATED ") 
-#pragma message("[VCGLIB Warning]  use vcg/simplex/vertexplus instead ")
 
 #ifndef VERTEX_TYPE 
-#pragma message("\nYou should never directly include this file\n")
+#pragma message("\nYou should never directly include this file\_n")
 #else
 
+#pragma message("VCGLIB Warning: this way to define the simplex vertex is DEPRECATED ")
+#pragma message("VCGLIB Warning: use vcg/simplex/vertexplus instead ")
 
 #include<vcg/space/point3.h>
 #include<vcg/space/color4.h>
