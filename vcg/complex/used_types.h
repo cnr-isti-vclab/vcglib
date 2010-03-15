2e739825c8f8f4360a3862ee59d3073b0c45385d
ganovelli <fabio.ganovelli@isti.cnr.it>
2010-03-15 13:10:43 +0000

    [ Changes in definition of  TriMesh: PART I ]
    Note for the developers: the change to make to existing projects is very little
    but strictly necessary to compile. This change IS NOT backward compliant.
    
    
    ==== OLD ==== way to define a TriMesh:
    
    // forward declarations
    class MyVertex;
    class MyEdge;
    class MyFace;
    
    class MyVertex: public VertexSimp2 < MyVertex, MyEdge, MyFace, vertex::Coord3f,...other components>{};
    class MyFace: public FaceSimp2 < MyVertex, MyEdge, MyFace, face::VertexRef,...other components>{};
    class MyMesh: public TriMesh<vector<MyVertex>,vector<MyFace> >{};
    
    
    ==== NEW ==== way to define a TriMesh:
    // forward declarations
    class MyVertex;
    class MyEdge;
    class MyFace;
    
    // declaration of which types is used as VertexType, which type is used as FaceType and so on...
    class MyUsedTypes: public vcg::UsedType < vcg::Use<MyVertex>::AsVertexType,
                                             vcg::Use<MyFace>::AsFaceType>{};
    
    class MyVertex: public Vertex < MyUsedTypes, vertex::Coord3f,...other components>{};
    class MyFace: public Face < MyUsedTypes, face::VertexRef,...other components>{};
    class MyMesh: public TriMesh<vector<MyVertex>,vector<MyFace> >{};
    
                                   ===== classes introduced         [vcg::UsedType] : it is a class containing all the types that must be passed to the definition of Vertex, Face, Edge... This
    class replaces the list of classes to pass as first templates and the need to specify the maximal simplicial. So
    
    < MyVertex, MyEdge, MyFace,  beocmes  MyUsedTypes,
    and
    VertexSimp2 becomes Vertex
    
    [vcg::Use] : an auxiliary class to give a simple way to specify the role of a type
    
    Note 2: the order of templates parameters to vcg::UsedTypes is unimportant, e.g:
    
    class MyUsedTypes: public vcg::UsedType <vcg::Use<MyVertex>::AsVertexType,
                                            vcg::Use<MyEdge>::AsEdgeType,
                                            vcg::Use<MyFace>::AsFaceType>{};
    
    is the same as:
    class MyUsedTypes: public vcg::UsedType <vcg::Use<MyFace>::AsFaceType,
                                            vcg::Use<MyEdge>::AsEdgeType,
                                            vcg::Use<MyVertex>::AsVertexType>{};
    
    ==== the Part II will be a tiny change to the class TriMesh it self.

---
+++ vcg/complex/used_types.h
@@ -0,0 +1,50 @@
+#ifndef VCG_USED_TYPES_H
+#define VCG_USED_TYPES_H
+
+#include <vcg/space/point3.h>
+#include <vcg/container/derivation_chain.h>
+
+namespace vcg{
+
+struct DummyTypes{
+		typedef char VertexType; 		// simplex types
+		typedef char EdgeType;
+		typedef char FaceType;
+		typedef char TetraType;
+		typedef char HEdgeType; 		// connector types
+
+		typedef vcg::Point3f CoordType; 		// connector types
+		typedef float ScalarType;						// connector types
+
+		typedef VertexType * VertexPointer;
+		typedef EdgeType *	EdgePointer		;
+		typedef FaceType * FacePointer		;
+		typedef TetraType * TetraPointer	;
+		typedef HEdgeType * HEdgePointer	;
+
+		static void Name(std::vector<std::string> & name){}
+		template < class LeftV>
+		void ImportLocal(const LeftV  & left ) {}
+};
+
+template <template <typename> class A = DefaultDeriver, template <typename> class B = DefaultDeriver,
+					template <typename> class C = DefaultDeriver, template <typename> class D = DefaultDeriver,
+					template <typename> class E = DefaultDeriver, template <typename> class F = DefaultDeriver,
+					template <typename> class G = DefaultDeriver, template <typename> class H = DefaultDeriver,
+					template <typename> class I = DefaultDeriver, template <typename> class J = DefaultDeriver,
+					template <typename> class K = DefaultDeriver, template <typename> class L = DefaultDeriver>
+					class UsedTypes: public Arity12<DummyTypes, A, B, C, D, E, F, G, H, I, J, K, L>  {};
+
+
+template <class A>
+	struct Use{
+		template <class T> struct AsVertexType: public T{typedef A VertexType;	typedef VertexType * VertexPointer	;};
+		template <class T> struct AsEdgeType: public T{typedef A EdgeType;			typedef EdgeType *	EdgePointer			;};
+		template <class T> struct AsFaceType: public T{typedef A FaceType;			typedef FaceType * FacePointer			;};
+		template <class T> struct AsTetraType: public T{typedef A TetraType;		typedef TetraType * TetraPointer		;};
+		template <class T> struct AsHEdgeType: public T{typedef A HEdgeType;		typedef HEdgeType * HEdgePointer		;};
+};
+
+};
+
+#endif // USED_TYPES_H
