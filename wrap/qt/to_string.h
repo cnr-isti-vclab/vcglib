0dbf1bf30575000456020fd13611b5a8c53267eb
cignoni <paolo.cignoni@isti.cnr.it>
2010-09-01 22:01:03 +0000

    missing vcg:: namespace in types...

--- wrap/qt/to_string.h
+++ wrap/qt/to_string.h
@@ -3,27 +3,27 @@
 #include <QString>
 #include <cstdlib>
 
-inline QString toString( const Point4f& p ){
+inline QString toString( const vcg::Point4f& p ){
   QString s;
   s.sprintf("%f %f %f %f", p[0], p[1], p[2], p[3]);
   return s;
 }
-inline QString toString( const Point3f& p ){
+inline QString toString( const vcg::Point3f& p ){
   QString s;
   s.sprintf("%f %f %f", p[0], p[1], p[2]);
   return s;
 }
-inline QString toString( const Point2f& p ){
+inline QString toString( const vcg::Point2f& p ){
   QString s;
   s.sprintf("%f %f", p[0], p[1]);
   return s;
 }
-inline QString toString( const Point2i& p ){
+inline QString toString( const vcg::Point2i& p ){
   QString s;
   s.sprintf("%d %d", p[0], p[1]);
   return s;
 }
-inline QString toString(Matrix44f& m){
+inline QString toString(vcg::Matrix44f& m){
   QString mat;
   for(int i=0; i<3; i++){
       mat.append( toString( m.GetRow4(i) ) );
