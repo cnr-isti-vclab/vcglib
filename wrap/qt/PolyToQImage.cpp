e5842d71d86ee698afa07a8991bfeca50cc6b067
cignoni <paolo.cignoni@isti.cnr.it>
2011-11-15 11:01:53 +0000

    Small changes to the utility function to save a bunch of 2 polygons onto a bitmap.

--- wrap/qt/PolyToQImage.cpp
+++ wrap/qt/PolyToQImage.cpp
@@ -6,24 +6,40 @@
 using namespace vcg;
 using namespace std;
 
-int dumpPolySet(const char * imageName,vector< vector<Point2f> > &polyVec, vector<Similarity2f> &trVec, int width, int height)
+int dumpPolySet(const char * imageName, vector< vector<Point2f> > &polyVec, int width, int height)
 {
+  Box2f bb;
+  for(size_t i=0;i<polyVec.size();++i)
+    for(int j=0;j<polyVec[i].size();++j)
+      bb.Add(polyVec[i][j]);
+
+  Similarity2f sim;
+  sim.sca = min(float(width)/bb.DimX(),float(height)/bb.DimY());
+  sim.tra = Point2f(width/2.0f,height/2.0f)-bb.Center()*sim.sca;
+  vector<Similarity2f> trVec(polyVec.size(),sim);
+
+  return dumpPolySet(imageName,polyVec,trVec,width,height);
+}
+
+int dumpPolySet(const char * imageName, vector< vector< vector<Point2f> > > &polyVecVec, vector<Similarity2f> &trVec, int width, int height)
+{
+  assert(polyVecVec.size() == trVec.size());
+
   QImage img(width,height,QImage::Format_RGB32);
   img.fill(qRgb(128,128,128));
   QPainter painter(&img);           // paint in picture
-  for(size_t i=0;i<polyVec.size();++i)
+  for(size_t i=0;i<polyVecVec.size();++i)
   {
     QVector<QPointF> ppQ;
-    for(int j=0;j<polyVec[i].size();++j)
+    for(int j=0;j<polyVecVec[i][0].size();++j)
     {
-      Point2f pp=trVec[i]*polyVec[i][j];
+      Point2f pp=trVec[i]*polyVecVec[i][0][j];
       ppQ.push_back(QPointF(pp[0],pp[1]));
     }
 
-    QBrush bb(vcg::ColorConverter::ToQColor(Color4b::Scatter(polyVec.size(),i)));
+    QBrush bb(vcg::ColorConverter::ToQColor(Color4b::Scatter(polyVecVec.size(),i)));
     painter.setBrush(bb);
     painter.drawPolygon(&*ppQ.begin(),ppQ.size(),Qt::OddEvenFill);
-
   }
   painter.end();
   img = img.mirrored(false,true);
@@ -35,5 +51,16 @@ int dumpPolySet(const char * imageName,vector< vector<Point2f> > &polyVec, vecto
       if(img.pixel(i,j) == qRgb(128,128,128)) emptyCnt++;
 
   return emptyCnt;
+}
+
+int dumpPolySet(const char * imageName, vector< vector<Point2f> > &polyVec, vector<Similarity2f> &trVec, int width, int height)
+{
+  vector< vector< vector<Point2f> > > polyVecVec(polyVec.size());
+  for(size_t i=0;i<polyVec.size();++i)
+  {
+    polyVecVec[i].resize(1);
+    polyVecVec[i][0]=polyVec[i];
+  }
+  dumpPolySet(imageName,polyVecVec,trVec,width,height);
 }
 
--- wrap/qt/PolyToQImage.h
+++ wrap/qt/PolyToQImage.h
@@ -1,8 +1,11 @@
 #ifndef POLYTOQIMAGE_H
 #define POLYTOQIMAGE_H
 #include <vcg/space/point2.h>
+#include <vcg/space/box2.h>
 #include <vcg/math/similarity2.h>
 
-int dumpPolySet(const char * imageName, std::vector< std::vector<vcg::Point2f> > &polyVec, std::vector<vcg::Similarity2f> &trVec,int width=1024,int height=1024);
+int dumpPolySet(const char * imageName, std::vector< std::vector< std::vector<vcg::Point2f> > > &polyVecVec, std::vector<vcg::Similarity2f> &trVec, int width, int height);
+int dumpPolySet(const char * imageName, std::vector< std::vector<vcg::Point2f> > &polyVec, std::vector<vcg::Similarity2f> &trVec, int width=1024,int height=1024);
+int dumpPolySet(const char * imageName, std::vector< std::vector<vcg::Point2f> > &polyVec, int width=1024,int height=1024);
 
 #endif // POLYTOQIMAGE_H
