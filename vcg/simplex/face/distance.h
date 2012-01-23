c32db09276b8dff692050e7dc943fb59cdd65dfd
cignoni <paolo.cignoni@isti.cnr.it>
2012-01-23 06:47:31 +0000

    Changed the name of the Distance from point to plane from DistancePointPlane to     SignedDistancePointPlane to underline the fact that it is SIGNED!!!

--- vcg/simplex/face/distance.h
+++ vcg/simplex/face/distance.h
@@ -105,7 +105,7 @@ namespace vcg {
                 //const ScalarType EPSILON = 0.00000001;
 		ScalarType b,b0,b1,b2;
 			// Calcolo distanza punto piano
-		ScalarType d = DistancePlanePoint( f.cPlane(), q );
+		ScalarType d = SignedDistancePlanePoint( f.cPlane(), q );
 		if( d>dist || d<-dist )			// Risultato peggiore: niente di fatto
 			return false;
 
