7cf395c033ceec0234d4084e9d87e479550c55e4
matteodelle <matteo.dellepiane@isti.cnr.it>
2012-09-03 09:35:17 +0000

    dellepiane: modified uint definition for compatibility with vstudio

--- wrap/gcache/cache.h
+++ wrap/gcache/cache.h
@@ -1,7 +1,19 @@
 #ifndef GCACHE_CACHE_H
 #define GCACHE_CACHE_H
 
+#ifdef _MSC_VER
+
+typedef __int16 int16_t;
+typedef unsigned __int16 uint16_t;
+typedef __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
+
+#else
 #include <stdint.h>
+#endif
+
 #include <iostream>
 #include <limits.h>
 #include <vector>
