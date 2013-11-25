54b288611a2b3f78cbd4b063cfbcaf289273636a
cignoni <paolo.cignoni@isti.cnr.it>
2013-11-25 10:38:17 +0000

    Include header cleaning and reordering.

--- vcg/complex/algorithms/parametrization/poisson_solver.h
+++ vcg/complex/algorithms/parametrization/poisson_solver.h
@@ -8,7 +8,7 @@
 *                                                                    \      *
 * All rights reserved.                                                      *
 *                                                                           *
-* This program is free software; you can redistribute it and/or modify      *   
+* This program is free software; you can redistribute it and/or modify      *
 * it under the terms of the GNU General Public License as published by      *
 * the Free Software Foundation; either version 2 of the License, or         *
 * (at your option) any later version.                                       *
@@ -29,7 +29,6 @@
 #include <eigenlib/unsupported/Eigen/SparseExtra>
 
 #include <time.h>
-#include <vcg/complex/allocate.h>
 #include <vcg/complex/algorithms/clean.h>
 #include <vcg/complex/algorithms/update/flag.h>
 #include <vcg/complex/algorithms/update/bounding.h>
