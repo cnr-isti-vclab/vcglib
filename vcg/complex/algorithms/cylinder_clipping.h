fd09d6fee27b6654af9ee3e7ce74ed1973b9b865
cignoni <paolo.cignoni@isti.cnr.it>
2013-11-28 23:39:45 +0000

    Added standard heading comment to the newly added file.

--- vcg/complex/algorithms/cylinder_clipping.h
+++ vcg/complex/algorithms/cylinder_clipping.h
@@ -1,3 +1,25 @@
+/****************************************************************************
+ * VCGLib                                                            o o     *
+ * Visual and Computer Graphics Library                            o     o   *
+ *                                                                _   O  _   *
+ * Copyright(C) 2004                                                \/)\/    *
+ * Visual Computing Lab                                            /\/|      *
+ * ISTI - Italian National Research Council                           |      *
+ *                                                                    \      *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * This program is free software; you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation; either version 2 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+ * for more details.                                                         *
+ *                                                                           *
+ ****************************************************************************/
 #ifndef CYLINDER_CLIP_H
 #define CYLINDER_CLIP_H
 #include <vcg/space/segment3.h>
@@ -234,7 +256,7 @@ public:
       // Now check also against the caps
       CoordType pt;
       if(IntersectionPlaneSegment(pl0,edgeSeg,pt)){
-        if((Distance(pt,origin)<radius+1.0*minEdgeLen) &&
+        if((Distance(pt,origin)<radius+2.0*minEdgeLen) &&
            (Distance(pt,p0)>eps) && (Distance(pt,p1)>eps) )
         {
               newPtMap[std::make_pair(p0,p1)] = pt;
@@ -242,7 +264,7 @@ public:
         }
       }
       if(IntersectionPlaneSegment(pl1,edgeSeg,pt)){
-        if( (Distance(pt,end)<radius+1.0*minEdgeLen) &&
+        if( (Distance(pt,end)<radius+2.0*minEdgeLen) &&
             (Distance(pt,p0)>eps) && (Distance(pt,p1)>eps) )
         {
               newPtMap[std::make_pair(p0,p1)] = pt;
