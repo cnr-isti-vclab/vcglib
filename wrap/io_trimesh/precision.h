adca0d032efed26eb710a78619c1d98065166f90
granzuglia <guido.ranzuglia@isti.cnr.it>
2014-10-06 08:27:24 +0000

    - added precision.h file

---
+++ wrap/io_trimesh/precision.h
@@ -0,0 +1,32 @@
+#ifndef __VCGLIB_PRECISION
+#define __VCGLIB_PRECISION
+
+namespace vcg
+{
+	namespace tri
+	{
+		namespace io
+		{
+			template<typename SCALAR>
+			struct Precision
+			{};
+			
+			template<>
+			struct Precision<float>
+			{
+				static int digits() {return 7;}
+                static const char* typeName() {return "float";}
+			};
+			
+			template<>
+			struct Precision<double>
+			{
+				static int digits() {return 16;}
+                static const char* typeName() {return "double";}
+			};
+		}
+	}
+}
+
+
+#endif
