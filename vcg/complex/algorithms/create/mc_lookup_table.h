a690c99fee4b0b1e17e79709fac0670be6699a89
cignoni <paolo.cignoni@isti.cnr.it>
2016-06-13 05:28:31 +0000

    Updated copyright disclaimer of the mc lookup table file after the explicit copyright transfer allowed by the original author of the file (Thomas Lewiner)

--- vcg/complex/algorithms/create/mc_lookup_table.h
+++ vcg/complex/algorithms/create/mc_lookup_table.h
@@ -1,25 +1,32 @@
-/**
-* @file    LookUpTable.h
-* @author  Thomas Lewiner <thomas.lewiner@polytechnique.org>
-* @author  Math Dept, PUC-Rio
-* @version 0.2
-* @date    12/08/2002
-*
-* @brief   LookUpTable for the MarchingCubes 33 Algorithm
-*                                                                           
-* This program is free software; you can redistribute it and/or modify      
-* it under the terms of the GNU General Public License as published by      
-* the Free Software Foundation; either version 2 of the License, or         
-* (at your option) any later version.                                       
-*                                                                           
-* This program is distributed in the hope that it will be useful,           
-* but WITHOUT ANY WARRANTY; without even the implied warranty of            
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             
-* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          
-* for more details.                                                         
-*/
-//________________________________________________
+/****************************************************************************
+* VCGLib                                                            o o     *
+* Visual and Computer Graphics Library                            o     o   *
+*                                                                _   O  _   *
+* Copyright(C) 2016                                                \/)\/    *
+* Visual Computing Lab                                            /\/|      *
+* ISTI - Italian National Research Council                           |      *
+*                                                                    \      *
+* All rights reserved.                                                      *
+*                                                                           *
+* This program is free software; you can redistribute it and/or modify      *   
+* it under the terms of the GNU General Public License as published by      *
+* the Free Software Foundation; either version 2 of the License, or         *
+* (at your option) any later version.                                       *
+*                                                                           *
+* This program is distributed in the hope that it will be useful,           *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+* GNU General Public License (http://www.gnu.org/licenses/gpl.txt)          *
+* for more details.                                                         *
+*                                                                           *
+****************************************************************************/
 
+/**
+Note: 
+The original version of this file was written by Thomas Lewiner. 
+Written copyright explicit transfer to the Visual Computing Lab in 2016 June.
+Current Copyright of this version of the file is of Visual Computing Lab.
+*/
 
 #ifndef __VCG_MC_LOOK_UP_TABLE
 #define __VCG_MC_LOOK_UP_TABLE
