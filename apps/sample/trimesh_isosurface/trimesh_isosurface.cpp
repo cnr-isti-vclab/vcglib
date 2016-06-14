cf308f9c2ee6b126775a339ee647e9c0f4f62014
Paolo Cignoni <paolo.cignoni@isti.cnr.it>
2016-06-14 21:36:42 +0200

    Corrected wrong include

--- apps/sample/trimesh_isosurface/trimesh_isosurface.cpp
+++ apps/sample/trimesh_isosurface/trimesh_isosurface.cpp
@@ -23,7 +23,6 @@
 #include <vcg/complex/complex.h>
 #include <vcg/math/perlin_noise.h>
 #include <vcg/complex/algorithms/create/marching_cubes.h>
-#include <vcg/complex/algorithms/create/extended_marching_cubes.h>
 #include <vcg/complex/algorithms/create/mc_trivial_walker.h>
 #include <wrap/io_trimesh/export_ply.h>
 
