fdfb58cf7e90341e6c18df9bc988c14ac6774e64
Luigi Malomo <malomo.luigi@gmail.com>
2018-12-14 16:43:31 +0100

    added eigen conversion to point2

--- vcg/space/deprecated_point2.h
+++ vcg/space/deprecated_point2.h
@@ -302,6 +302,18 @@ public:
 	{
 		_v[0] = b.X(); _v[1] = b.Y();
 	}
+	template <class EigenVector>
+	inline void FromEigenVector(const EigenVector & b)
+	{
+		_v[0] = ScalarType(b[0]);
+		_v[1] = ScalarType(b[1]);
+	}
+	template <class EigenVector>
+	inline void ToEigenVector(EigenVector & b) const
+	{
+		b[0]=_v[0];
+		b[1]=_v[1];
+	}
 		/// constructs a 2D points from an existing one of different type
 	template <class T>
 	static Point2 Construct( const Point2<T> & b )
