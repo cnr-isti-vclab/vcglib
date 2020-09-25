91859b946a8c161281b56164e9683645c0078fe7
P.M <60963077+goodengineer@users.noreply.github.com>
2020-09-25 16:17:30 +0300

    Update trimesh_isosurface.cpp

--- apps/sample/trimesh_isosurface/trimesh_isosurface.cpp
+++ apps/sample/trimesh_isosurface/trimesh_isosurface.cpp
@@ -25,6 +25,9 @@
 #include <vcg/complex/algorithms/create/marching_cubes.h>
 #include <vcg/complex/algorithms/create/mc_trivial_walker.h>
 #include <wrap/io_trimesh/export_ply.h>
+#include <wrap/io_trimesh/export_stl.h>
+#include <wrap/io_trimesh/export_obj.h>
+#include <wrap/io_trimesh/export_off.h>
 
 using namespace std;
 using namespace vcg;
@@ -34,42 +37,47 @@ typedef float ScalarType;
 class MyFace;
 class MyVertex;
 
-struct MyUsedTypes : public UsedTypes<	Use<MyVertex>		::AsVertexType,
-                                                                                Use<MyFace>			::AsFaceType>{};
+struct MyUsedTypes : public UsedTypes<	Use<MyVertex>::AsVertexType,Use<MyFace>::AsFaceType>{};
 
 class MyVertex     : public Vertex< MyUsedTypes, vertex::Coord3f, vertex::Normal3f, vertex::BitFlags>{};
 class MyFace       : public Face< MyUsedTypes, face::VertexRef, face::BitFlags> {};
-
-class MyMesh		: public vcg::tri::TriMesh< std::vector< MyVertex>, std::vector< MyFace > > {};
-
-
+class MyMesh	   : public vcg::tri::TriMesh< std::vector< MyVertex>, std::vector< MyFace > > {};
 
 typedef SimpleVolume<SimpleVoxel<float> > MyVolume;
 
+#define SZ 256
+#define NOISE_COEFF 2
+#define LEN 45
+
 int main(int /*argc*/ , char **/*argv*/)
 {
-    MyVolume	volume;
+  MyVolume	volume;
 
   typedef vcg::tri::TrivialWalker<MyMesh,MyVolume>	MyWalker;
   typedef vcg::tri::MarchingCubes<MyMesh, MyWalker>	MyMarchingCubes;
   MyWalker walker;
 
-
-  // Simple initialization of the volume with some cool perlin noise
+  // Simple initialization of the volume with some perlin noise
   vcg::Box3f bb(vcg::Point3f(-1,-1,-1),vcg::Point3f(1,1,1));
-  volume.Init(Point3i(64,64,64),bb);
-  for(int i=0;i<64;i++)
-    for(int j=0;j<64;j++)
-      for(int k=0;k<64;k++)
-        volume.Val(i,j,k)=(j-32)*(j-32)+(k-32)*(k-32)  + i*10*(float)math::Perlin::Noise(i*.2,j*.2,k*.2);
 
+  volume.Init(Point3i(SZ,SZ,SZ),bb);
+  for(short i=0;i<SZ;i++)
+    for(short j=0;j<SZ;j++)
+      for(short k=0;k<SZ;k++)
+        volume.Val(i,j,k)=(j-SZ/2)*(j-SZ/2)+(k-SZ/2)*(k-SZ/2)  + i*NOISE_COEFF*(float)math::Perlin::Noise(i*.2,j*.2,k*.2);
 
 	// MARCHING CUBES
 	MyMesh		mc_mesh;
-	printf("[MARCHING CUBES] Building mesh...");
-	MyMarchingCubes					mc(mc_mesh, walker);
-	walker.BuildMesh<MyMarchingCubes>(mc_mesh, volume, mc, 20*20);
-	vcg::tri::io::ExporterPLY<MyMesh>::Save( mc_mesh, "marching_cubes.ply");
+        short mask = 0;
 
-	printf("OK!\n");
+	printf("[MARCHING CUBES] Building mesh...");
+	MyMarchingCubes	mc(mc_mesh, walker);
+        walker.BuildMesh<MyMarchingCubes>(mc_mesh, volume, mc, LEN*LEN);
+
+    vcg::tri::io::ExporterPLY<MyMesh>::Save( mc_mesh, "marching_cubes.ply");
+    vcg::tri::io::ExporterSTL<MyMesh>::Save( mc_mesh, "marching_cubes.stl");
+    vcg::tri::io::ExporterOBJ<MyMesh>::Save( mc_mesh, "marching_cubes.obj",mask);
+    vcg::tri::io::ExporterOFF<MyMesh>::Save( mc_mesh, "marching_cubes.off");
+	
+    printf("OK!\n");
 };
