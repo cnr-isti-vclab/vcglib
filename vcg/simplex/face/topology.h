4b2455fad40969b2aa55771afc3653888df60f7f
nico <nico.pietroni@gmail.com>
2020-12-11 14:35:56 +1100

    corrected ccw order of faces for border vertices

--- vcg/simplex/face/topology.h
+++ vcg/simplex/face/topology.h
@@ -1194,7 +1194,8 @@ void VFOrderedStarFF(const Pos<FaceType> &startPos,
 {
 	VFOrderedStarFF(startPos, posVec);
 	const auto & pos = posVec[0];
-	if (ccw != (pos.VFlip() == pos.F()->V(pos.F()->Prev(pos.VInd()))))
+    //if (ccw != (pos.VFlip() == pos.F()->V(pos.F()->Prev(pos.VInd()))))
+    if ((ccw) == (pos.V()!=pos.F()->V(pos.E())))
 	{
 		std::reverse(posVec.begin(), posVec.end());
 	}
