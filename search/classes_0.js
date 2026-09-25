ba45c3baf068515051a2ab235825d0d56eb246dd
cignoni <paolo.cignoni@cnr.it>
2026-09-25 08:19:29 +0000

    Deploying to gh-pages from @ cnr-isti-vclab/vcglib@f978a89f3a28e684b3f1bfa6c0a752813d29a95a 🚀

--- search/classes_0.js
+++ search/classes_0.js
@@ -3,5 +3,7 @@ var searchData=
   ['allocator_0',['Allocator',['../classvcg_1_1tri_1_1Allocator.html',1,'vcg::tri']]],
   ['anisotropicdistance_1',['AnisotropicDistance',['../classvcg_1_1tri_1_1AnisotropicDistance.html',1,'vcg::tri']]],
   ['append_2',['Append',['../classvcg_1_1tri_1_1Append.html',1,'vcg::tri']]],
-  ['areadata_3',['AreaData',['../classvcg_1_1tri_1_1UpdateCurvature_1_1AreaData.html',1,'vcg::tri::UpdateCurvature']]]
+  ['areadata_3',['AreaData',['../classvcg_1_1tri_1_1UpdateCurvature_1_1AreaData.html',1,'vcg::tri::UpdateCurvature']]],
+  ['attributeedgepredicate_4',['AttributeEdgePredicate',['../classvcg_1_1tri_1_1AttributeEdgePredicate.html',1,'vcg::tri']]],
+  ['attributemidpointfunctor_5',['AttributeMidPointFunctor',['../classvcg_1_1tri_1_1AttributeMidPointFunctor.html',1,'vcg::tri']]]
 ];
